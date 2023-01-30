300063e9b57db8154887f98874fdf8264b081a5e
Otavio Marques <marquesotavio251204@gmail.com>
2023-01-30 13:27:57 -0300

    metodo post

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const app = express()
 
 app.listen('3000')
 
-app.route('/').get((req, res)=>{//função do express que inicia a rota
+/*app.route('/').get((req, res)=>{//função do express que inicia a rota
     //req = requisição; res = resposta; .send = enviar
     res.send('hello')
 })
 
 app.route('/sobre').get((req, res)=>{
     res.send('sobre')
+})*/
+
+//middleware
+app.use(express.json())//transformando tudo em json
+
+app.route('/').post((req, res)=> {
+    res.send(req.body)
 })
