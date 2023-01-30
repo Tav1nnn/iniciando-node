5e8c14c9a9d5a6b850748e561a4391f92e538f1e
Otavio Marques <marquesotavio251204@gmail.com>
2023-01-30 13:56:03 -0300

    metodo delete

--- index.js
+++ index.js
@@ -6,23 +6,26 @@ app.listen('3000')
 
 
 //GET
-/*app.route('/').get((req, res)=>{//função do express que inicia a rota
+app.route('/').get((req, res)=>{//função do express que inicia a rota
     //req = requisição; res = resposta; .send = enviar
     res.send('hello')
 })
 
 app.route('/sobre').get((req, res)=>{
     res.send('sobre')
-})*/
+})
 
+/*===========================================================*/
 
 //POST
 //middleware
-/*app.use(express.json())//transformando tudo em json
+app.use(express.json())//transformando tudo em json
 
 app.route('/').post((req, res)=> {
     res.send(req.body)
-})*/
+})
+
+/*===========================================================*/
 
 //PUT
 //editar informações
@@ -38,4 +41,14 @@ app.route('/').put((req, res) => {
     author = req.body.author
     res.send(author)
     console.log(author);
+})
+
+/*===========================================================*/
+
+//DELETE
+// : fala que é uma variavel
+// params vem todas as variaveis da url
+app.route('/:id').delete((req,res)=>{
+    author = ''
+    res.send(req.params.id)
 })
