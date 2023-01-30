e8f3d845a5740f5d57f1c3279ecdc910d9b1e31d
Otavio Marques <marquesotavio251204@gmail.com>
2023-01-29 22:05:27 -0300

    metodo get

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const app = express()
 
 app.listen('3000')
 
- 
+app.route('/').get((req, res)=>{//função do express que inicia a rota
+    //req = requisição; res = resposta; .send = enviar
+    res.send('hello')
+})
+
+app.route('/sobre').get((req, res)=>{
+    res.send('sobre')
+})
