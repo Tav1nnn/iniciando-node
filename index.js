1233571dc807c28181d650230a0d042ce554fccd
Otavio Marques <marquesotavio251204@gmail.com>
2023-01-30 13:43:05 -0300

    metodo put

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 
 app.listen('3000')
 
+
+//GET
 /*app.route('/').get((req, res)=>{//função do express que inicia a rota
     //req = requisição; res = resposta; .send = enviar
     res.send('hello')
@@ -13,9 +15,27 @@ app.route('/sobre').get((req, res)=>{
     res.send('sobre')
 })*/
 
+
+//POST
 //middleware
-app.use(express.json())//transformando tudo em json
+/*app.use(express.json())//transformando tudo em json
 
 app.route('/').post((req, res)=> {
     res.send(req.body)
+})*/
+
+//PUT
+//editar informações
+
+let author = 'otavio'
+app.use(express.json())
+
+app.route('/').get((req, res) => {
+    res.send(author)
+})
+
+app.route('/').put((req, res) => {
+    author = req.body.author
+    res.send(author)
+    console.log(author);
 })
